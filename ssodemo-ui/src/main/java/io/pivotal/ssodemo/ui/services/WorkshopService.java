29e1f7c3f6c30460a91832605b08f4aa99aaa9f0
Chris Decelles <cdecelles@pivotal.io>
2017-09-21 14:37:36 -0400

    new files

---
+++ ssodemo-ui/src/main/java/io/pivotal/ssodemo/ui/services/WorkshopService.java
@@ -0,0 +1,12 @@
+package io.pivotal.ssodemo.ui.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WorkshopService {
+
+	private static Logger log = LoggerFactory.getLogger(WorkshopService.class);
+
+}
